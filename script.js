1705cad828e19dffec1fa08b14fafa833ccd4664
fbdthree <112531356+fbdthree@users.noreply.github.com>
2022-10-28 13:26:05 -0400

    made page dynamic - added button - added local storage

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ for(var i = 9; i <= 17; i++) {
         timeBlk = (i - 12) + " pm"
     }
   htmlBlocks = ` <div class="row">
-  <h5 class="col-md-1">${timeBlk}</h5>
-  <textarea class="col-md-10"rows="2" id="${i}" cols="100"></textarea>
-  <button class="btn btn-primary saveplan col-md-1">Save</button>
+  <h5 class="col-sm-1 col-md-1">${timeBlk}</h5>
+  <textarea class="col-sm-9 col-md-10"rows="2" id="${i}" cols="100"></textarea>
+  <button class="btn btn-primary saveplan col-sm-2 col-md-1">Save</button>
  </div>`
  $("#timeblock").append(htmlBlocks) // creating time block
 }
@@ -27,3 +27,11 @@ for(var i = 9; i <= 17; i++) {
 //user ="X"
 // console.log("Welcome"+user)
 //console.log(`Welcome ${user}`)
+
+
+$(".saveplan").on("click",function(){
+    var time = $(this).siblings("textarea").attr("id")
+    var userInput = $(this).siblings("textarea").val()
+    console.log("btn",time,userInput)
+    localStorage.setItem(time, userInput)
+})

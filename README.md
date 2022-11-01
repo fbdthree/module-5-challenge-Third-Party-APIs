19059f35b3746d192256796ba8c5b9d332918058
fbdthree <112531356+fbdthree@users.noreply.github.com>
2022-10-31 23:39:35 -0400

    added a README file

---
+++ README.md
@@ -0,0 +1,18 @@
+#  **Work Day Scheduler**
+
+## **Introduction**
+
+The purpose of the *Work Day Scheduler* is to provide a web-based scheduling appication that give that user the abilitiy to schedule their workday.
+
+## **Project Description**
+
+The *Work Day Scheduler* is a dynamic webpage that displays every hour in the workday, 9:00 AM to 5:00 PM, and gives the user the ability to add notes to each hour of the day, creating a schedule.  A "Save" button is provided alongside the corresponding notes, giving the user the opportunity to save the notes to the browser's local storage.  The header of the page shows the current date, time, and year.
+
+## **Technologies Used**
+
+  * HTML
+  * CSS
+  * JavaScript
+  * jQuery
+
+  
--- script.js
+++ script.js
@@ -1,37 +1,44 @@
+// declare variable for time-blocks
+var htmlBlocks ="";
 
-// jQuery and moment are linked
-
-// 1- build the time blocks and append to body
-
-// 2- 
-
-var htmlBlocks =""
+// build time-blocks one at a time useing a for-loop
+// each iteration appends a row with columns for time, notes, and a save button
 for(var i = 9; i <= 17; i++) {
     var timeBlk;
     if(i < 12){
-        timeBlk = i +" am"
+        timeBlk = i +" am";
     }else if(i  === 12){
-        timeBlk = i + " pm"
+        timeBlk = i + " pm";
     }else{
-        timeBlk = (i - 12) + " pm"
+        timeBlk = (i - 12) + " pm";
     }
   htmlBlocks = ` <div class="row">
   <h5 class="col-sm-1 col-md-1">${timeBlk}</h5>
   <textarea class="col-sm-9 col-md-10"rows="2" id="${i}" cols="100"></textarea>
   <button class="btn btn-primary saveplan col-sm-2 col-md-1">Save</button>
  </div>`
- $("#timeblock").append(htmlBlocks) // creating time block
+ $("#timeblock").append(htmlBlocks); 
 }
 
+
+//line 24
 //template literals
 //user ="X"
 // console.log("Welcome"+user)
 //console.log(`Welcome ${user}`)
 
-
+// create clickable event for save button
 $(".saveplan").on("click",function(){
-    var time = $(this).siblings("textarea").attr("id")
-    var userInput = $(this).siblings("textarea").val()
-    console.log("btn",time,userInput)
-    localStorage.setItem(time, userInput)
+    var time = $(this).siblings("textarea").attr("id");
+    var userInput = $(this).siblings("textarea").val();
+    console.log("btn",time,userInput);
+    localStorage.setItem(time, userInput);
 })
+
+
+
+setInterval(() => {
+    var now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $('#currentDay').text(now);
+    
+}, 1000);
